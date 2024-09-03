fee272e0c96ad07abaee72035e91d62091d71fc6
GUSTAVO <Gustavoandressanchezceron@gmail.com>
2024-09-02 21:43:01 -0500

    cambios-loging

--- app/src/main/java/com/example/splashscreen/screens/LoginScreen.kt
+++ app/src/main/java/com/example/splashscreen/screens/LoginScreen.kt
@@ -1,22 +1,30 @@
 package com.example.splashscreen.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.splashscreen.R
 import com.example.splashscreen.navigation.NavigationItem
 
 @Composable
@@ -27,35 +35,57 @@ fun LoginScreen(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Bienvenido!", style = MaterialTheme.typography.bodyLarge)
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo Emprende Link",
+            modifier = Modifier.size(200.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "¡BIENVENIDO!",
+            style = MaterialTheme.typography.bodyLarge,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
             value = "",
             onValueChange = { },
-            label = { Text("Usuario")},
+            label = { Text("Usuario") },
             singleLine = true
         )
         Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
             value = "",
             onValueChange = { },
-            label = { Text("Contraseña")},
+            label = { Text("Contraseña") },
             visualTransformation = PasswordVisualTransformation(),
             singleLine = true
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "¿Olvidaste tu contraseña?",
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.Gray
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text("Iniciar Sesion")
-        }
         Button(
-            onClick = {
-                navController.navigate( // Navigate to a route in the current NavGraph
-                    NavigationItem.Home.route // Route defined in AppNavHost
-                ) }
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Black)
         ) {
-            Text("Registrarse")
+            Text("Iniciar Sesión", color = Color.White)
         }
 
+        Button(
+            onClick = {
+                navController.navigate(
+                    NavigationItem.Register.route
+                )
+            },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Black)
+        ) {
+            Text("Registrarse", color = Color.White)
+        }
     }
 }
 
