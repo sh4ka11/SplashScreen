e6bece1939d2041702789785376e136f58d46bab
Sebastian-Paz <esdp5432@gmail.com>
2024-10-27 20:01:27 +1400

    commit

--- app/src/main/java/com/example/splashscreen/screens/buscar_emprendimientos_inversionista.kt
+++ app/src/main/java/com/example/splashscreen/screens/buscar_emprendimientos_inversionista.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.splashscreen.R
 
+
 data class Emprendimientos(
     val nombre: String,
     val descripcion: String,
